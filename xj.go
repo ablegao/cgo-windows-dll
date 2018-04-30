95b323780cc7782c2c2d96fa23b10a39d404c2e0
Able Gao <ablegao@gmail.com>
2018-05-01 02:16:25 +0800

    fix 内存溢出问题

--- xj.go
+++ xj.go
@@ -1,31 +1,5 @@
 package main
 
-/*
-#include "stdio.h"
-#include "stdlib.h"
-#include <windows.h>
-
-typedef int(*hand)(char*,char*);
-HINSTANCE dll;
-hand hisinterface;
-char* output;
-
-void cInit(char* path){
- dll = LoadLibrary(path);
- hisinterface = (hand)GetProcAddress(dll,"hisinterface");
-}
-void Free(){
-    hisinterface = NULL;
-    FreeLibrary(dll);
-}
-int cFunc( char * input , char * output){
-  if(hisinterface==NULL){
-    printf("%s","output;;;;;;; hisinterface is NULL");
-    return -1;
-  }
-  return hisinterface(input,output);
-}
-*/
 import "C"
 
 import (
@@ -39,6 +13,8 @@ import (
 	"syscall"
 	"unsafe"
 
+	"runtime"
+
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 )
@@ -47,21 +23,33 @@ var (
 	debugData = flag.String("d", "", "Command data")
 	serverT   = flag.String("s", "0.0.0.0:8000", "Address, if none , run in termail!")
 	dllPath   = flag.String("p", "./", "Dll path .")
+	cFunc     *syscall.Proc
 )
 
 func StrPtr(s string) uintptr {
 	return uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(s)))
 }
 func loadFunc(body string) string {
-	output := C.CString(" ")
-	//defer C.free(unsafe.Pointer(output))
-	data, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader([]byte(body)), simplifiedchinese.GBK.NewEncoder()))
-	stdin := C.CString(string(data))
-	defer C.free(unsafe.Pointer(output))
-	defer C.free(unsafe.Pointer(stdin))
-	C.cFunc(stdin, output)
 
-	outdata, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader([]byte(C.GoString(output))), simplifiedchinese.GBK.NewDecoder()))
+	// 声明一快内存空间, 必须明确大小，否则会因为编码规范差异造成溢出。
+	out := make([]byte, 1024)
+	// 取出指针地址
+	p := &out[0]
+	//输入数据转换成GBK编码
+	input, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader([]byte(body)), simplifiedchinese.GBK.NewEncoder()))
+
+	//调用dll 反馈，  uintptr(unsafe.Pointer(p)) 将指针传给dll ,dll会给指针赋值.
+	cFunc.Call(StrPtr(string(input)), uintptr(unsafe.Pointer(p)))
+	output := []byte{}
+	for _, x := range out {
+		if x != 0 {
+			output = append(output, x)
+		} else {
+			break
+		}
+	}
+	//将结果转成UTF-8输出
+	outdata, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader(output), simplifiedchinese.GBK.NewDecoder()))
 	return string(outdata)
 }
 
@@ -80,14 +68,16 @@ func runHttp(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	runtime.GOMAXPROCS(1)
 	flag.Parse()
-	C.cInit(C.CString(*dllPath + "hisinterface.dll"))
 
+	//加载dll
+	dll := syscall.MustLoadDLL(*dllPath + "hisinterface.dll")
+	//加载 dll 函数
+	cFunc = dll.MustFindProc("hisinterface")
 	if *serverT == "" {
 		fmt.Print(loadFunc(*debugData))
-		//C.Free()
 	} else {
-		defer C.Free()
 		log.Println("Host:", *serverT)
 		http.HandleFunc("/api", runHttp)
 		log.Fatal(http.ListenAndServe(*serverT, nil))
